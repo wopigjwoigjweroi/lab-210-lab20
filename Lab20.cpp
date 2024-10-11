b09f572be90eba58af7057c2e23600f81fdb375e
wopigjwoigjweroi <agizekel@gmail.com>
2024-10-10 19:15:56 -0700

    Commit 1

--- Lab20.cpp
+++ Lab20.cpp
@@ -1,4 +1,27 @@
 #include <iostream> 
-
+#include <cstdlib> 
 using namespace std; 
 
+const int SIZE = 3; 
+
+class Chair {
+
+    private: 
+
+        int legs; 
+        double *prices; 
+
+    public:
+
+        Chair() {
+
+            legs = (rand() % 2) + 3; 
+
+            prices = new double[SIZE]; 
+
+            for (int i = 0; i < SIZE; i++) {
+
+                prices[i] = (rand() % (9999 - 100 + 1) + 10000) / 100.0; 
+            } 
+        }
+};
