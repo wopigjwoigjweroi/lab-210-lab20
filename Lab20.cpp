7e4c2545047d15f3eb291a95421349d94957d666
wopigjwoigjweroi <agizekel@gmail.com>
2024-10-10 19:26:57 -0700

    Commit 2

--- Lab20.cpp
+++ Lab20.cpp
@@ -24,4 +24,33 @@ class Chair {
                 prices[i] = (rand() % (9999 - 100 + 1) + 10000) / 100.0; 
             } 
         }
+
+         Chair(int l, double a[]) {
+
+            prices = new double[SIZE]; 
+
+            legs = l; 
+
+            for (int i = 0; i < SIZE; i++) {
+
+                prices[i] = a[i]; 
+            } 
+        }
+
+        ~Chair() {
+
+            delete[] prices; 
+        }
+
+        void setLegs(int l) {
+
+            legs = 1; 
+        }
+
+        int getLegs() {
+
+            return legs; 
+        }
+
+        
 };
